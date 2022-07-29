0950a730c508043da144c0bd5dccefd04a836aea
Cry0ne <ss421299@gmail.com>
2022-07-29 18:57:25 +0300

    Заменил js логику.

--- src/components/ui/spoiler/spoiler.ts
+++ src/components/ui/spoiler/spoiler.ts
@@ -2,19 +2,38 @@ import Component, { ComponentProps } from '@/base/component';
 
 export default class Spoiler extends Component {
 
+    head?: HTMLElement;
+    collapse?: HTMLElement;
+    isOpen: boolean;
+    
     constructor(element: ComponentProps) {
         super(element);
-        this.nRoot.children[0].addEventListener('click', this.clickHandler);        
-    } 
-
-    clickHandler = (e:Event) =>{
-        const spoiler = (<HTMLElement>e.target).closest('.spoiler');
-        if(!spoiler) return;
-
-        spoiler.classList.toggle("is-open");
-        const spoilerCollapse = spoiler.children[1] as HTMLElement;
-        spoilerCollapse.style.height = spoiler.classList.contains('is-open') 
-            ? `${spoilerCollapse.scrollHeight}px` 
-            : spoilerCollapse.style.height = '0';
+        
+        this.head = this.getElement('head');
+        this.collapse = this.getElement('collapse');
+        this.isOpen = false;
+        
+        if(this.head)this.head.addEventListener('click', this.clickHandler);
     }
+    
+    clickHandler = () => {
+        this.isOpen ? this.close() : this.open();
+    }
+    
+    open = () => {
+        this.nRoot.classList.add("is-open");
+        if(this.collapse)this.collapse.style.height = `${this.collapse.scrollHeight}px`;
+        this.isOpen = true;
+    }
+
+    close = () => {
+        this.nRoot.classList.remove("is-open");
+        if(this.collapse)this.collapse.style.height = '0px';
+        this.isOpen = false;
+    }
+
+
+
+
+
 }
